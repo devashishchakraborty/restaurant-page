6d8c8939099701b3b9a0c83f15ccac41866d772b
Devashish Chakraborty <devashishchakra@gmail.com>
2023-05-31 01:05:01 +0530

    Added navbar and title in js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 const header = document.createElement("header");
 const content = document.querySelector("#content");
 
-header.textContent = "Garuda Vegetarian Restaurant";
+const title = document.createElement("div");
+title.setAttribute("class", "title");
+
+const navbar = document.createElement("nav");
+
+const homeTab = document.createElement("div");
+homeTab.setAttribute("class", "home");
+
+const menuTab = document.createElement("div");
+menuTab.setAttribute("class", "menu");
+
+const contactTab = document.createElement("div");
+contactTab.setAttribute("class", "contact");
+
+title.textContent = "Garuda Vegetarian Restaurant";
+homeTab.textContent = "HOME";
+menuTab.textContent = "MENU";
+contactTab.textContent = "CONTACT";
+
+
+navbar.appendChild(homeTab);
+navbar.appendChild(menuTab);
+navbar.appendChild(contactTab);
+
+content.appendChild(header);
+
+header.appendChild(title);
+header.appendChild(navbar);
