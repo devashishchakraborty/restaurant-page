09c7d2ddc875de9532d0c2a67a6845cd043d257b
Devashish Chakraborty <devashishchakra@gmail.com>
2023-06-06 01:22:57 +0530

    Added a module to populate the home page

---
+++ src/home.js
@@ -0,0 +1,7 @@
+function displayHome(main) {
+    const home = document.createElement("div");
+    home.textContent = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere deserunt quae beatae accusamus porro officia error, quisquam veniam minus odio temporibus vitae consequuntur tenetur praesentium dolore consectetur, ab, hic ea."
+    main.appendChild(home);
+}
+
+export { displayHome }
